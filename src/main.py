0231e8f1a605bd5352ac178251ef5048c7af9f72
jackmcmaken <jackmcmaken9@gmail.com>
2023-11-20 16:39:46 -0500

    Add project structure, graphics lib tbd

--- src/main.py
+++ src/main.py
@@ -3,12 +3,13 @@ import dearpygui.dearpygui as dpg
 dpg.create_context()
 dpg.create_viewport(title='Custom Title', width=900, height=900)
 
-with dpg.window(label="Example Window"):
+with dpg.window(label="Example Window", no_title_bar=True, no_resize=True, no_move=True):
     dpg.add_text("Hello, world")
     dpg.add_button(label="Save")
     dpg.add_input_text(label="string", default_value="Quick brown fox")
     dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
 
+dpg.set_primary_window("Example Window", True)
 dpg.setup_dearpygui()
 dpg.show_viewport(maximized=True)
 dpg.start_dearpygui()
