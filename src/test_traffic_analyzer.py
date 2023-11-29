2f2533e950aa4bf250bcf32f5eb4cfa880676f3e
jackmcmaken <jackmcmaken9@gmail.com>
2023-11-29 01:52:18 -0500

    + tests for packet_csv and netx visualization

--- src/test_traffic_analyzer.py
+++ src/test_traffic_analyzer.py
@@ -1,3 +1,14 @@
 import src.network.traffic_analyzer
+#from scapy.all import *
+import networkx as nx
+import matplotlib.pyplot as plt
 
-src.network.traffic_analyzer.getPackets()
+
+#src.network.traffic_analyzer.getPackets()
+
+netcsv = src.network.traffic_analyzer.packet_csv()
+network = src.network.traffic_analyzer.visualizeNet(netcsv)
+print("Nodes:", network.nodes())
+print("Edges:", network.edges())
+nx.draw_circular(network, with_labels = True)
+plt.show()
