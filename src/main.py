5fcfa5f032b22d74135f973f05213a42740eca38
jackmcmaken <jackmcmaken9@gmail.com>
2024-02-13 16:10:37 -0500

    Fix layout

--- src/main.py
+++ src/main.py
@@ -4,40 +4,51 @@ import dash_bootstrap_components as dbc
 from dash import Dash, html, callback, dcc, Input, Output
 import networkx as nx
 from dash.dash import no_update
+import dash_cytoscape as cyto
 from networkx.readwrite import json_graph
 import src.utils.visualization as vs
 
-# setup style
+# Setup style
 external_stylesheets = [dbc.themes.DARKLY]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-# get network information from modules
+# Get network information from modules
 net = pr.PacketData()
 net.capture_packets()
 net_csv = pr.csv_format(net.tcp_packet_data)
-# print(net_csv)
-# print(ta.get_common_dst(net_csv))
 net.tcp_packet_data.show()
 pcapfile = net.writeFile()
 
-# setup app layout with net info
-app.layout = html.Div([
-    html.Div(children=[
-        html.H3('Network Analyzer'), 
-        html.Hr()
+# App layout
+app.layout = dbc.Container(fluid=True, children=[
+    dbc.Row(justify="center", align="center", className="mb-3", children=[
+        dbc.Col(md=8, children=[
+            html.H3('Network Analyzer', className='text-center mb-4'), 
+            html.Hr()
+        ])
     ]),
-    html.Div(children=[
-        html.P('Found ' + str(len(net_csv)) + ' TCP packets.'), 
-        html.Br(), 
-        html.P('The most common destination was ' + str(ta.get_common_dst(net_csv)))
+    dbc.Row(justify="center", align="center", className="mb-3", children=[
+        dbc.Col(md=8, children=[
+            dbc.Alert(f'Found {len(net_csv)} TCP packets.', color="info", className="mb-3"),
+            dbc.Alert(f'The most common destination was {ta.get_common_dst(net_csv)}', color="success"),
+        ])
     ]),
-    html.Div(children=[
-        html.H4('Network Visualization'), 
-        dcc.Graph(figure=vs.graph_figure(net_csv))
+    dbc.Row(justify="center", align="center", className="mb-3", children=[
+        dbc.Col(md=8, children=[
+            html.H4('Network Visualization', className='mb-4'), 
+            cyto.Cytoscape(
+                id='network-graph',
+                elements=vs.graph2(),
+                style={'width': '100%', 'height': '400px'},
+                layout={'name': 'cose'}
+            )
+        ])
     ]),
-    html.Div(children=[
-        html.Button('Download pcap file', id='download-button'), 
-        dcc.Download(id='download', data={'filename': '_dash_no_update'})
+    dbc.Row(justify="center", align="center", className="mb-3", children=[
+        dbc.Col(md=8, children=[
+            dbc.Button('Download pcap file', id='download-button', color="primary", className="me-md-2"), 
+            dcc.Download(id='download')
+        ])
     ])
 ])
 
@@ -53,9 +64,8 @@ def func(n_clicks):
     with open(pcapfile, "rb") as file:
         pcap_content = file.read()
     
-    return dcc.send_file(pcap_content, "example")
+    return dcc.send_bytes(pcap_content, "network_capture.pcap")
 
-# run app
+# Run app
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run_server(debug=True)
