f2f36eaf0d5f7304323105c549887af3c4fb5016
jackmcmaken <jackmcmaken9@gmail.com>
2024-02-13 16:11:32 -0500

    Add test 2nd graph, fake packets

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -29,3 +29,33 @@ def graph_figure(net_csv):
         },
     }
     return graph
+
+def graph2():
+    G = nx.Graph()
+
+    # example
+    packets = [
+        {'src': '192.168.1.1', 'dst': '192.168.1.2'},
+        {'src': '192.168.1.1', 'dst': '192.168.1.3'},
+        {'src': '192.168.1.2', 'dst': '192.168.1.3'},
+    ]
+
+    # Add nodes and edges
+    for packet in packets:
+        src = packet['src']
+        dst = packet['dst']
+        if not G.has_node(src):
+            G.add_node(src)
+        if not G.has_node(dst):
+            G.add_node(dst)
+        if not G.has_edge(src, dst):
+            G.add_edge(src, dst, weight=0)
+        G[src][dst]['weight'] += 1
+        
+    elements = [
+        {'data': {'id': node, 'label': node}} for node in G.nodes
+    ] + [
+        {'data': {'source': source, 'target': target, 'weight': G[source][target]['weight']}}
+        for source, target in G.edges
+    ]
+    return elements
